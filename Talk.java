07d9cdbd715fcc8945c569a37c49ff4c2fe246cc
NazarPtashynskyi <nazarptashynskyy@gmail.com>
2018-06-29 07:52:43 +0300

    Add Talk.java

---
+++ Talk.java
@@ -0,0 +1,19 @@
+package weekone.lesson1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Talk{
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("What is your name?");
+		String name = br.readLine();
+		System.out.println("Where are you live, " + name + " ?");
+		String place = br.readLine();
+		System.out.println("Wow " + name + ", u live in " + place + ", it is very cool place");
+
+	}
+
+}
